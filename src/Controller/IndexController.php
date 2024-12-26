5bf063cf2a236fa2a8261fba9144a50c9409f685
TutoDevWeb <TutoDevWeb@outlook.fr>
2024-12-26 15:37:25 +0100

    J'ai terminé une première version de mon projet.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,13 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class IndexController extends AbstractController
-{
-    #[Route('/index', name: 'app_index')]
-    public function index(): Response
-    {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
-    }
+class IndexController extends AbstractController {
+	#[Route('/', name: 'index')]
+	public function index(): Response {
+		return $this->render('index/index.html.twig');
+	}
 }
